1f28569330bf6433472e520d2ada78468c0319bb
s160421001 <s160421001@student.ubaya.ac.id>
2023-04-18 12:33:37 +0700

    input.php

--- input.php
+++ input.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+if(!isset($_COOKIE['remember']))
+    {
+        header("location: setting.php");
+    }  
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +16,17 @@
     <title>Input Data</title>
 </head>
 <body>
-    
+    <form method="POST" action="display.php" enctype="multipart/form-data">
+        <p><label>NRP *</label><input type="text" name="txtnrp" value="" required></p>
+        <p><label>Nama *</label><input type="text" name="txtnama" value="" required></p>
+        <p><label>Alamat </label><textarea name="txtalamat"></textarea></p>
+        <p><label>IPK *</label><input type="number" step="any" name="txtipk" min="0" max="4" required></p>
+        <p><input type="submit" name="btnsimpan" value="Simpan"></p>
+    </form>
+
+    <?php
+        $test = array("nrp" => $_POST['txtnrp'], "nama" => $_POST['txtnama'], "alamat" => $_POST['txtalamat'], "ipk" => $_POST['txtipk']);
+        $_SESSION['user'] = $test;
+    ?>
 </body>
 </html>
